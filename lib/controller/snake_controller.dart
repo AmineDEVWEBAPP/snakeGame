5ebd734bd12613e32333aac5852ef568e660cffd
khadirAmine <mohammedaminekhadir6@gmail.com>
2025-09-06 13:04:28 +0100

    fix bug (snake_controll)

--- lib/controller/snake_controller.dart
+++ lib/controller/snake_controller.dart
@@ -16,8 +16,10 @@ class SnakeController extends GetxController {
   late Deriction _deriction;
   // the first item in pixels is head and last item is tail
   late List<int> pixels;
+  // height is the pixels withode headPixel
+  int _height = 1;
   late int ball;
-  late List<Deriction> _footPrint;
+  late List<int> _pixelPrint;
   late int speed;
   late int head;
   late int tail;
@@ -35,13 +37,14 @@ class SnakeController extends GetxController {
     pixels = [_allPixels.first];
     head = pixels.first;
     tail = pixels.last;
+    // init pixelPrint
+    _pixelPrint = [head];
     // get random ball pixels
     ball = _allPixels[1];
     //get random deriction
     List<int> deriLengths = [0, 1, 2, 3];
     deriLengths.shuffle();
     _deriction = Deriction.values[deriLengths[0]];
-    _footPrint = [_deriction];
   }
 
   void reInit() {
@@ -76,18 +79,15 @@ class SnakeController extends GetxController {
 
   Future<void> updateLocation() async {
     while (_gContr.isStarting) {
-      // logger head deriction for author pixels follow head
+      // logger head Pixel for body pixel following the prints
       _loggerPrints();
-      // move each pixel by for loop
-      for (int i = 0; i < pixels.length; i++) {
-        // if the pixel is in outSide appears in opposide side
-        _outSide(_deriction, i);
-        // move the pixel
-        _movePixel(i == 0 ? _deriction : _footPrint[i], i);
-        update(['pixel']);
-      }
+      // check if the pixel is in outSide appears in opposide side
+      _outSide();
+      // move the pixel
+      _movePixels();
       // when the headPixel == ballPixel add a pixel to snakePixels
-      _increaseHieght(_guide);
+      _increaseHieght();
+      update(['pixel']);
       // value head and tail pixels
       head = pixels.first;
       tail = pixels.last;
@@ -98,19 +98,30 @@ class SnakeController extends GetxController {
     }
   }
 
-  void _increaseHieght(int pixel) {
+  void _loggerPrints() {
+    _pixelPrint.insert(0, head);
+    if (_pixelPrint.length > _height) {
+      _pixelPrint.removeLast();
+    }
+  }
+
+  void _increaseHieght() {
     if (pixels.first == ball) {
       _changeBallLocation();
       _gContr.points++;
       _gContr.update(['info']);
-      int addNum = pixels.last - _guide;
-      pixels.add(addNum);
+      int afterLastPixel =
+          pixels[pixels.lastIndexOf(pixels.last) - (pixels.length > 1 ? 1 : 0)];
+      int lastGuide = afterLastPixel - pixels.last;
+      int addPixel = pixels.last - lastGuide;
+      pixels.add(addPixel);
+      _height = pixels.length;
     }
   }
 
-  void _movePixel(Deriction snakeDeri, int pixelIndex) async {
+  void _movePixels() async {
     // value the guide to move the pixel
-    switch (snakeDeri) {
+    switch (_deriction) {
       case Deriction.top:
         _guide = -_verticalPixel;
         break;
@@ -123,47 +134,48 @@ class SnakeController extends GetxController {
       case Deriction.right:
         _guide = _horizontalPixel;
     }
-    // move the pixel
-    pixels[pixelIndex] += _guide;
+    // move head pixel
+    pixels[0] += _guide;
+    // move body pixels after head Pixel by for loop for following head pixels Print
+    for (int i = 1; i < _height; i++) {
+      pixels[i] = _pixelPrint[i - 1];
+    }
   }
 
-  void _outSide(Deriction snakeDeri, int pixelIndex) {
+  void _outSide() {
+    // sides nums
     List<int> rightNums = List.generate(15, (i) => (15 * i) + 14);
     List<int> leftNums = List.generate(15, (i) => 15 * i);
     List<int> topNums = List.generate(15, (i) => i);
     List<int> bottomNums = List.generate(15, (i) => i + 210);
-
-    switch (snakeDeri) {
-      case Deriction.top:
-        if (topNums.contains(pixels[pixelIndex])) {
-          pixels[pixelIndex] += 225;
-        }
-        break;
-      case Deriction.bottom:
-        if (bottomNums.contains(pixels[pixelIndex])) {
-          pixels[pixelIndex] -= 225;
-        }
-        break;
-      case Deriction.left:
-        if (leftNums.contains(pixels[pixelIndex])) {
-          pixels[pixelIndex] += 15;
-        }
-        break;
-      case Deriction.right:
-        if (rightNums.contains(pixels[pixelIndex])) {
-          pixels[pixelIndex] -= 15;
-        }
-    }
-  }
-
-  void _loggerPrints() {
-    _footPrint.insert(0, _deriction);
-    if (_footPrint.length > pixels.length) {
-      _footPrint.removeLast();
+    // check each pixel by for loop if is it in outside
+    for (int i = 0; i < _height; i++) {
+      switch (_deriction) {
+        case Deriction.top:
+          if (topNums.contains(pixels[i])) {
+            pixels[i] += 225;
+          }
+          break;
+        case Deriction.bottom:
+          if (bottomNums.contains(pixels[i])) {
+            pixels[i] -= 225;
+          }
+          break;
+        case Deriction.left:
+          if (leftNums.contains(pixels[i])) {
+            pixels[i] += 15;
+          }
+          break;
+        case Deriction.right:
+          if (rightNums.contains(pixels[i])) {
+            pixels[i] -= 15;
+          }
+      }
     }
   }
 
   void _loss() async {
+    // get pixels widhoud head Pixel
     List<int> pixels = this.pixels
         .skipWhile((item) => item == this.pixels[0])
         .toList();
